121367d7829cbd0e5d59d13fc658156bb67922c1
Jordan Heider <jheider@KG-JHEIDER03.local>
2024-04-02 13:19:07 -0500

    Fixed piece removal, added intro, changed styles and colors. Additional fixes.

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ const answers = [
     'six',
     'mask',
     'power play',
-    'penalty kill'
+    'penalty kill',
+    'hanging'
 ];
 
 const questions = [
@@ -50,12 +51,13 @@ const questions = [
   'The name of the trophy won by the NHL team that wins the league championship each year. [2]',
   'How many players are on the ice at one time for a team? [1]',
   '1974 was the last year that goalies didn’t wear a what? [1]',
-  'what its called one team has more players on the ice due to a penalty [2]',
-  'what its called when one team has less players due to a penalty [2]'
+  'What its called one team has more players on the ice due to a penalty [2]',
+  'What its called when one team has less players due to a penalty [2]',
+  'Ruth\'s punishment for murdering Sydney Snow[1]'
 ];
 
 //Pieces Array
-const pieces = [
+let pieces = [
   "p1","p2","p3","p4","p5","p6","p7","p8","p9","p10",
   "p11","p12","p13","p14","p15","p16","p17","p18","p19","p20",
   "p21","p22","p23","p24","p25","p26","p27","p28","p29","p30",
@@ -76,10 +78,8 @@ function checkAnswer(answer, answers){
   if ( (answers.indexOf(answer) > -1) ) {
     data.right = 1; //mark as right
     data.wrong = 0; //mark as wrong
-    console.log('data.right: ', data.right);
-    console.log('data.wrong: ', data.wrong);
-    data.pieces.splice(randomIntFromInterval(0, 39),1);
-    data.pieces.splice(randomIntFromInterval(0, 39),1);
+    data.pieces.splice(randomArray(data.pieces),1);
+    data.pieces.splice(randomArray(data.pieces),1);
     return true
   }
   else { 
@@ -89,22 +89,20 @@ function checkAnswer(answer, answers){
   }
 }
 
-//Return Random Number Between Two Numbers
-function randomIntFromInterval(min, max) { // min and max included 
-  return Math.floor(Math.random() * (max - min + 1) + min)
+//Pick Random Number From Array
+function randomArray(namedArray) {
+  return Math.floor( Math.random() * namedArray.length ); 
 }
 
 //Display Intro
 app.get('/', (req,res) => {
-    res.render('puzzle.ejs', data);
+    res.render('intro.ejs', data);
 });
 
 //Display Question by ID
 app.get("/clue/:id", (req, res) => {
   let id = req.params.id; //get ID from path
   data.id.splice(0,1,id); //Add ID to ID array (so form can read it)
-  console.log('REQ.PARAMS.ID: ',id);
-  console.log('DATA.ID: ',data.id);
   res.render("puzzle.ejs",data);
   data.right = 0; //reset
   data.wrong = 0; //reset
@@ -116,19 +114,24 @@ app.post("/submit/:id", (req, res) => {
   let question_field = "question_" + id;
   const answer = req.body[question_field];
   if (checkAnswer(answer,answers)) {
-    console.log("ANSWER: ", answer);
-    res.render("puzzle.ejs",data);
+    res.render("puzzle-right.ejs",data);
   }
   else {
-    res.render("puzzle.ejs",data);
+    res.render("puzzle-wrong.ejs",data);
   }
   });
 
-//Clear
-app.get("/clear/", (req, res) => {
+//Reset
+app.get("/reset/", (req, res) => {
   data.right = 0; //reset
   data.wrong = 0;
   data.id = [0];
+  data.pieces = [
+    "p1","p2","p3","p4","p5","p6","p7","p8","p9","p10",
+    "p11","p12","p13","p14","p15","p16","p17","p18","p19","p20",
+    "p21","p22","p23","p24","p25","p26","p27","p28","p29","p30",
+    "p31","p32","p33","p34","p35","p36","p37","p38","p39","p40",
+  ];
   res.render("puzzle.ejs",data);
 });
 
